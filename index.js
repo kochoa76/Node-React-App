d824df05e26d96a5289299f230174d954d1b55f0
github username <github email address>
2018-12-18 19:24:49 -0600

    add first half of google oath authentication

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
+
 const app = express();
 
-//route handler
-app.get('/', (req, res) => {
-  res.send({bye: 'friend'});
-});
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+  console.log(accessToken);
+  })
+);
+
 
 
 
