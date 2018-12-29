c18b6598211c5a9efc6b6326b526db087206fdb3
github username <github email address>
2018-12-28 20:45:38 -0600

    add mongoose to persist google data to mongoDB to allow authentication

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose')
 const keys = require('../config/keys');
+
+const User = mongoose.model('users')
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
@@ -8,9 +11,15 @@ passport.use(new GoogleStrategy({
   callbackURL: '/auth/google/callback'
 },
 (accessToken, refreshToken, profile, done) => {
-  console.log('accessToken', accessToken);
-  console.log('refreshToken', refreshToken);
-  console.log('profile:', profile);
+  User.findOne({googleId: profile.id}).then((existingUser) => {
+    if (existingUser) {
+      done(null, existingUser);
+    }else {
+      new User({googleId: profile.id})
+      .save()
+      .then(user => done(null, user));
+    }
+  })
   }
-)
+  )
 );
