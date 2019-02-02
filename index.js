ce3ec373d3213803f74ac3b6f0cde976fb369307
github username <github email address>
2019-02-01 21:57:51 -0600

    changes to node_env to NODE_ENV

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
+const keys = require('./config/keys')
 require('./models/User');
 require('./services/passport');
 
 
-const keys = require('./config/keys')
 mongoose.connect(keys.mongoURI);
 
 const app = express();
