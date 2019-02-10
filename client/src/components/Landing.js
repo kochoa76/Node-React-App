3323de0072552575da769908525276b0ec674ad0
github username <github email address>
2019-02-10 13:26:33 -0600

    adding routes for login and logout

---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Landing = () => {
+
+  return (
+    <div style={{ textAlign: 'center'}}>
+      <h1>
+        EFeedback!
+      </h1>
+      Collect Feedback from your users
+    </div>
+  )
+}
+
+export default Landing;
