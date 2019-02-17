4c3a25735d9913a2fbee3e81d0bf58dc36637d69
github username <github email address>
2019-02-17 17:54:07 -0600

    cleaning up Add payments button

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -7,9 +7,16 @@ class Payments extends Component {
 
     return (
       <StripeCheckout
+      name="EFeedback"
+      description="$5 for 5 email credits"
       amount= {500}
       token={token=> console.log(token)}
-      stripeKey={process.env.REACT_APP_STRIPE_KEY} />
+      stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">
+          Add credits
+        </button>
+      </StripeCheckout>
     )
   }
 }
